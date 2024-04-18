390c8476266a77e0798a65015ff45c376a16e1e6
b06611033 <messiharden@gmail.com>
2024-04-18 00:54:23 -0500

    implemented search and range-search

--- BplusTree.java
+++ BplusTree.java
@@ -7,25 +7,29 @@ public class BplusTree {
         this.order = order;
         root = new Node(true);
         for(int record: records) {
-            System.out.println("inserting: " + record);
-            insert(root, record);
-            System.out.println("--------------------------------------");
+            insert(root, record, false);
         }
     }
 
-    private Sibling insert(Node curr, int record) {
+    // ----------------------------------------------------- INSERT -------------------------------------------------------------------------
+    private Sibling insert(Node curr, int record, boolean debug) {
         // current node is leaf
         if(curr.isLeaf) {
-            insertToLeaf(curr, record);
+            insertToLeaf(curr, record, debug);
             // no overflow
             if(curr.keys.size() <= order) {
-                System.out.println("no overflow in leaf");
-                System.out.println("current leaf node keys: " + curr.keys);
+                if(debug) {
+                    System.out.println("no overflow in leaf");
+                    System.out.println("current leaf node keys: " + curr.keys);
+                }
                 return null;
             }
             else {
                 // create new leaf
-                System.out.println("splitting leaf node due to overflow");
+                if(debug) {
+                    System.out.println("splitting leaf node due to overflow");
+                    System.out.println("leaf node before splitting: " + curr.keys);
+                }
                 Node newNode = new Node(true);
                 int index = (order+1)/2;
                 List<Integer> newKeys = new LinkedList<>();
@@ -45,37 +49,46 @@ public class BplusTree {
                 pointers.add(newNode); // points to splitted node at right
                 newNode.keys = newKeys;
                 newNode.pointers = newPointers;
-                System.out.println("current leaf node: " + curr);
-                System.out.println("sibling leaf node: " + newNode);
-                System.out.println("current leaf node keys: " + curr.keys);
-                System.out.println("sibling leaf node keys: " + newNode.keys);
-                System.out.println("current leaf node points to : " + curr.pointers.get(0));
-                System.out.println("sibling leaf node points to: " + newNode.pointers);
-
-                if(curr == root) createRoot(curr, newNode, keyForParent);
+                if(debug) {
+                    System.out.println("leaf node after splitting: " + curr);
+                    System.out.println("sibling leaf node after splitting: " + newNode);
+                    System.out.println("leaf node keys after splitting: " + curr.keys);
+                    System.out.println("sibling leaf node keys after splitting: " + newNode.keys);
+                    System.out.println("leaf node points to after splitting: " + curr.pointers.get(0));
+                    System.out.println("sibling leaf node points to after splitting: " + newNode.pointers);
+                }
+                if(curr == root) createRoot(curr, newNode, keyForParent, debug);
                 return new Sibling(newNode, keyForParent);
             }
         }
         // node is not a leaf
+        if(debug) {
+            System.out.println("current non-leaf node: " + curr);
+            System.out.println("current non-leaf node keys: " + curr.keys);
+        }
         List<Integer> keys = curr.keys;
         List< Node> pointers = curr.pointers;
         Sibling newChild = null;
         for(int i = 0; i < keys.size(); i++) {
             if(keys.get(i) > record) {
-                newChild = insert(pointers.get(i), record);
+                newChild = insert(pointers.get(i), record, debug);
                 break;
             }
-            if(i == keys.size()-1)  newChild = insert(pointers.get(i+1), record);
+            if(i == keys.size()-1)  newChild = insert(pointers.get(i+1), record, debug);
         }
         if (newChild == null) return null;
         else if(keys.size() < order) {
-            insertToNonLeaf(curr, newChild);
+            insertToNonLeaf(curr, newChild, debug);
             return null;
         }
         else {
             // create new non-leaf node
-            System.out.println("splitting non-leaf node due to overflow");
-            insertToNonLeaf(curr, newChild);
+            insertToNonLeaf(curr, newChild, debug);
+            if(debug) {
+                System.out.println("splitting non-leaf node due to overflow");
+                System.out.println("non-leaf node before splitting: " + curr);
+                System.out.println("non-leaf node keys before splittinh: " + curr.keys);
+            }
             Node newNode = new Node(false);
             int middleKeyIndex = (order+1)/2-1;
             if((order+1)%2 == 1) middleKeyIndex++;
@@ -95,20 +108,24 @@ public class BplusTree {
             }
             newNode.keys = newKeys;
             newNode.pointers = newPointers;
-            System.out.println("current non-leaf node: " + curr);
-            System.out.println("sibling non-leaf node: " + newNode);
-            System.out.println("current non-leaf node keys: " + curr.keys);
-            System.out.println("sibling non-leaf node keys: " + newNode.keys);
-            System.out.println("current non-leaf node points to : " + curr.pointers);
-            System.out.println("sibling non-leaf node points to: " + newNode.pointers);
-
-            if(curr == root) createRoot(curr, newNode, keyForParent);
+            if(debug) {
+                System.out.println("non-leaf node after splitting: " + curr);
+                System.out.println("sibling non-leaf node after splitting: " + newNode);
+                System.out.println("non-leaf node keys after splitting: " + curr.keys);
+                System.out.println("sibling non-leaf node keys after splitting: " + newNode.keys);
+                System.out.println("non-leaf node points to after splitting: " + curr.pointers);
+                System.out.println("sibling non-leaf node points to after splitting: " + newNode.pointers);
+            }
+            if(curr == root) createRoot(curr, newNode, keyForParent, debug);
             return new Sibling(newNode, keyForParent);
         }
     }
 
-    private void insertToNonLeaf(Node curr, Sibling s) {
-        System.out.println("inserting to non-leaf");
+    private void insertToNonLeaf(Node curr, Sibling s, boolean debug) {
+        if(debug) {
+            System.out.println("inserting to non-leaf");
+            System.out.println("non-leaf node keys before insertion: " + curr.keys);
+        }
         int index = 0;
         List<Integer> keys = curr.keys;
         while(index < keys.size()) {
@@ -118,11 +135,17 @@ public class BplusTree {
         keys.add(index, s.key);
         List<Node> pointers = curr.pointers;
         pointers.add(index+1, s.n);
-        System.out.println("current non-leaf node keys: " + curr.keys);
-        System.out.println("current non-leaf node pointers: " + curr.pointers);
+        if(debug) {
+            System.out.println("non-leaf node keys after insertion: " + curr.keys);
+            System.out.println("non-leaf node pointers after insertion: " + curr.pointers);
+        }
     }
 
-    private void insertToLeaf(Node curr, int record) {
+    private void insertToLeaf(Node curr, int record, boolean debug) {
+        if(debug) {
+            System.out.println("inserting to leaf");
+            System.out.println("leaf node keys before insertion: " + curr.keys);
+        }
         int index = 0;
         List<Integer> keys = curr.keys;
         while(index < keys.size()) {
@@ -130,33 +153,147 @@ public class BplusTree {
             index++;
         }
         keys.add(index, record);
+        if(debug) {
+            System.out.println("leaf node keys after insertion: " + curr.keys);
+            System.out.println("leaf node pointers after insertion: " + curr.pointers);
+        }
     }
 
-    private void createRoot(Node curr, Node newNode, int firstSecondHalfKey) {
-        System.out.println("Creating new root");
+
+    private void createRoot(Node curr, Node newNode, int firstSecondHalfKey, boolean debug) {
+        if(debug) System.out.println("Creating new root");
         Node newRoot = new Node(false);
         newRoot.keys.add(firstSecondHalfKey);
         newRoot.pointers.add(curr);
         newRoot.pointers.add(newNode);
         root = newRoot;
-        System.out.println("new root: " + root);
-        System.out.println("root node keys: " + root.keys);
-        System.out.println("root node points to children 1: " + root.pointers.get(0));
-        System.out.println("root node points to children 2: " + root.pointers.get(1));
+        if(debug) {
+            System.out.println("new root: " + root);
+            System.out.println("root node keys: " + root.keys);
+            System.out.println("root node points to children 1: " + root.pointers.get(0));
+            System.out.println("root node points to children 2: " + root.pointers.get(1));
+        }
     }
 
+    // ----------------------------------------------------- DELETE -------------------------------------------------------------------------
 
-    //MAIN FUNCTION
+    // ----------------------------------------------------- SEARCH & RANGE SEARCH -------------------------------------------------------------------------
+    private String search(int key) {
+        System.out.println("-----searching data---------");
+        Node curr = root;
+        while(!curr.isLeaf) {
+            System.out.println("current non-leaf node is: " + curr);
+            System.out.println("keys of current non-leaf node are: " + curr.keys);
+            List<Integer> keys = curr.keys;
+            List<Node> pointers = curr.pointers;
+            boolean found = false;
+            for(int i = 0; i < keys.size(); i++) {
+                if(key < keys.get(i)) {
+                    curr = pointers.get(i);
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) curr = pointers.get(pointers.size()-1);
+        }
+        System.out.println("current leaf node is: " + curr);
+        System.out.println("keys in leaf node are: " + curr.keys);
+        for(int k: curr.keys) {
+            if(k == key) {
+                return ("data is: " + k + ", found in node: " + curr);
+            }
+        }
+        return "Data not found!!!";
+    }
+
+    private List<Integer> rangeSearch(int low, int high) {
+        System.out.println("-----range searching data---------");
+        Node curr = root;
+        while(!curr.isLeaf) {
+            System.out.println("current non-leaf node is: " + curr);
+            System.out.println("keys of current non-leaf node are: " + curr.keys);
+            List<Integer> keys = curr.keys;
+            List<Node> pointers = curr.pointers;
+            boolean found = false;
+            for(int i = 0; i < keys.size(); i++) {
+                if(low < keys.get(i)) {
+                    curr = pointers.get(i);
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) curr = pointers.get(pointers.size()-1);
+        }
+        List<Integer> data = new LinkedList<>();
+        while(curr != null) {
+            System.out.println("current leaf node is: " + curr);
+            System.out.println("keys in leaf node are: " + curr.keys);
+            for(int k: curr.keys) {
+                if(k >= low && k <= high) {
+                    data.add(k);
+                }
+                else if(k > high) {
+                    return data;
+                }
+            }
+            if(curr.pointers.size() == 0) return data;
+            else curr = curr.pointers.get(0);
+        }
+        return data;
+    }
+
+    // ----------------------------------------------------- HELPER FUNCTIONS -------------------------------------------------------------------------
+    public void printLeaf() {
+        System.out.println("-----printing leaf nodes---------");
+        Node curr = root;
+        while(!curr.isLeaf) curr = curr.pointers.get(0);
+        int count = 1;
+        while(curr.pointers.size() > 0) {
+            System.out.println("current node is: " + curr);
+            System.out.println("keys in the node are: " + curr.keys);
+            curr = curr.pointers.get(0);
+            count++;
+        }
+        System.out.println("current node is: " + curr);
+        System.out.println("keys in the node are: " + curr.keys);
+        System.out.println("total number of leaves: " + count);
+        System.out.println("-----finished printing---------");
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+     //GENERATE RECORDS
+     private static Set<Integer> generate(int num, int low) {
+        Set<Integer> records = new HashSet<>();
+        int curr = low;
+        for(int i = 0; i < num; i++) {
+            records.add(curr);
+            curr += 10;
+        }
+        return records;
+    }
+
+    // ----------------------------------------------------- MAIN FUNCTION -------------------------------------------------------------------------
     public static void main (String[] args) {
-        //Set<Integer> records = generate(10000, 100000);
-        //int order = 13;
         // sparse tree: insert record from small to large
         // dense tree: ??
-        List<Integer> records = new ArrayList<>(Arrays.asList(0,2,8,3,4,7,11,9,5,6,1,10,14,12,13,-2,-1));
-        BplusTree bt = new BplusTree(records, 4);
+        List<Integer> records = new ArrayList<>(generate(10000, 100000));
+        //List<Integer> records = new ArrayList<>(Arrays.asList(0,2,8,3,4,7,11,9,5,6,1,10,14,12,13));
+        BplusTree bt = new BplusTree(records, 24);
+        bt.insert(bt.getRoot(), 15, true);
+        System.out.println("---------------------------------");
+        bt.insert(bt.getRoot(), 199999, true);
+        System.out.println("---------------------------------");
+        bt.insert(bt.getRoot(), 155001, true);
+        System.out.println("---------------------------------");
+        //bt.printLeaf();
+        System.out.println(bt.search(17));
+        System.out.println(bt.rangeSearch(154999, 156580));
     }
 
-    //CLASSES
+    // ----------------------------------------------------- CLASSES -------------------------------------------------------------------------
     class Node {
         List<Integer> keys;
         List<Node> pointers;
@@ -177,15 +314,6 @@ public class BplusTree {
         }
     }
 
-     //GENERATE RECORDS
-     private static Set<Integer> generate(int num, int low) {
-        Set<Integer> records = new HashSet<>();
-        int curr = low;
-        for(int i = 0; i < num; i++) {
-            records.add(curr);
-            curr += 10;
-        }
-        return records;
-    }
+    // ----------------------------------------------------- TESTS -------------------------------------------------------------------------
 
 }
